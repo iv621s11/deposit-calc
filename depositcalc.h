586e4cd05396fa89e4615c318c99651d994d0322
DeMoNNsK <sib98@list.ru>
2017-03-29 16:53:58 +0700

    Added new function in depositcalc.h: "summa"

--- depositcalc.h
+++ depositcalc.h
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #ifndef DEPOSITCALC_H
 #define DEPOSITCALC_H
 
@@ -14,6 +15,54 @@ int check(int term, int startup)
 		printf("Неправильно введен срок вклада");
 		return 0;
 	}
+	return 1;
 }
 
-
+float summa(int term, int startup)
+{
+	float summa;
+	
+	if(term < 31)
+	{
+		summa = startup * -0.1;
+		return summa;
+	}
+	
+	if(startup < 100000)
+	{
+		if(term < 121)
+		{
+			summa = startup * 0.02 / 365 * term;
+		}
+		
+		else if(term < 241)
+		{
+			summa = startup * 0.06 / 365 * term;
+		}
+		
+		else if(term < 366)
+		{
+			summa = startup * 0.12 / 365 * term;
+		}
+	} 
+	else if(startup >= 100000)
+	{
+		  if(term < 121)
+		{
+			summa = startup * 0.03 / 365 * term;
+		}
+		
+		else if(term < 241)
+		{
+			summa = startup * 0.08 / 365 * term;
+		}
+		
+		else if(term < 366)
+		{
+			summa = startup * 0.15 / 365 * term;
+		}
+		
+	}
+	return summa;
+}
+#endif
