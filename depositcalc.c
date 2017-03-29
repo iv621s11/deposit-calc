8bbbd2602770dcb7e7e47b6e4f056392c3e00ab7
DeMoNNsK <sib98@list.ru>
2017-03-29 15:33:57 +0700

    Added full-work main function

--- depositcalc.c
+++ depositcalc.c
@@ -2,5 +2,17 @@
 
 int main()
 {
+	int startup, int term;
+	
+	printf("Введите сумму вклада: ");
+	scanf("%d", &startup)
+	
+	printf("Введите срок вклада: ");
+	scanf("%d", &term);
+	
+	Check(term, startup);
+	
+	printf("%d", summa(startup, term);
+	
 	return 0;
 }
