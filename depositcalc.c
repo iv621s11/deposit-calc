737041ee6ac89aed537e47d4e8e2a2371f32dee5
DeMoNNsK <sib98@list.ru>
2017-03-29 16:54:41 +0700

    Fix errors in main function

--- depositcalc.c
+++ depositcalc.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 #include "depositcalc.h"
 
+int check(int term, int startup);
+float summa(int term, int startup);
+
 int main()
 {
-	int startup, int term;
+	int startup, term;
 	
 	printf("Введите сумму вклада: ");
-	scanf("%d", &startup)
+	scanf("%d", &startup);
 	
 	printf("Введите срок вклада: ");
 	scanf("%d", &term);
 	
-	Check(term, startup);
-	
-	printf("%d", summa(startup, term);
-	
-	return 0;
+	if(check(term, startup) == 1)
+	{
+		printf("Ваш доход со вклада составит: %.2f\n", summa(term, startup));
+		return 0;
+	}
+	else 
+	return -1;
 }
