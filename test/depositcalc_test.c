9abfec5dec0b8b98b9dc4b6683b2fbaef1597ec8
Козлов Дмитрий Игоревич <Sib98@list.ru>
2017-05-13 13:03:24 +0700

    Added first test file for function "check"

--- test/depositcalc_test.c
+++ test/depositcalc_test.c
@@ -1,8 +1,111 @@
-#define CTEST_MAIN
-
+#include <depositcalc.h>
 #include <ctest.h>
 
-int main(int argc, const char** argv)
-{
-    return ctest_main(argc, argv);
+CTEST(term_check_test, test1)
+{	
+	int term = 0;
+	int startup = 150000;
+	
+	int result = check(term, startup);
+	
+	int expected = 	1;
+	
+	ASSERT_EQUAL(expected, result);
 }
+
+CTEST(term_check_test, test2)
+{	
+	int term = -50;
+	int startup = 150000;
+	
+	int result = check(term, startup);
+	
+	int expected = 	0;
+	
+	ASSERT_EQUAL(expected, result);
+}
+
+CTEST(term_check_test, test3)
+{	
+	int term = 365;
+	int startup = 150000;
+	
+	int result = check(term, startup);
+	
+	int expected = 	1;
+	
+	ASSERT_EQUAL(expected, result);
+}
+
+CTEST(check_test, test4)
+{	
+	int term = 367;
+	int startup = 150000;
+	
+	int result = check(term, startup);
+	
+	int expected = 	1;
+	
+	ASSERT_EQUAL(expected, result);
+}
+
+CTEST(startup_check_test, test1)
+{	
+	int term = 0;
+	int startup = -15;
+	
+	int result = check(term, startup);
+	
+	int expected = 	0;
+	
+	ASSERT_EQUAL(expected, result);
+}
+
+CTEST(startup_check_test, test2)
+{	
+	int term = 0;
+	int startup = 0;
+	
+	int result = check(term, startup);
+	
+	int expected = 	0;
+	
+	ASSERT_EQUAL(expected, result);
+}
+
+CTEST(startup_check_test, test3)
+{	
+	int term = 0;
+	int startup = 1000;
+	
+	int result = check(term, startup);
+	
+	int expected = 	0;
+	
+	ASSERT_EQUAL(expected, result);
+}
+
+CTEST(startup_check_test, test4)
+{	
+	int term = 0;
+	int startup = 10000;
+	
+	int result = check(term, startup);
+	
+	int expected = 	1;
+	
+	ASSERT_EQUAL(expected, result);
+}
+
+CTEST(startup_check_test, test5)
+{	
+	int term = 0;
+	int startup = 1000000;
+	
+	int result = check(term, startup);
+	
+	int expected = 	1;
+	
+	ASSERT_EQUAL(expected, result);
+}
+
